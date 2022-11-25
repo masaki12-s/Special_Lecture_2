9eff562bc7d4f46be1ee083399c8d0561e3c8827
masaki12-s <70832772+masaki12-s@users.noreply.github.com>
2022-11-25 16:35:15 +0900

    パスを絶対パスに変更

--- tests/sample_test.py
+++ tests/sample_test.py
@@ -5,15 +5,15 @@ from speciallecture.CSVPrinter import CSVPrinter
 
 class TestCSVPrinter(unittest.TestCase):
     def test_read1(self):
-        printer = CSVPrinter("./sample.csv")
+        printer = CSVPrinter("/home/runner/work/Special_Lecture_2/Special_Lecture_2/tests/sample.csv")
         l = printer.read()
         self.assertEqual(3, len(l))
         # self.assertRaises()
     def test_read2(self):
-        printer = CSVPrinter("./sample.csv")
+        printer = CSVPrinter("/home/runner/work/Special_Lecture_2/Special_Lecture_2/tests/sample.csv")
         l = printer.read()
         print(l)
     def test_read3(self):
         with self.assertRaises(FileNotFoundError):
-            printer = CSVPrinter("./sample2.csv")
+            printer = CSVPrinter("/home/runner/work/Special_Lecture_2/Special_Lecture_2/test/sample2.csv")
             l = printer.read()
